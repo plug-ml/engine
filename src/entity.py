634241d282c4c892a2b74104152023686fbd09e1
Gabriel Lee <gelos@localhost.localdomain>
2021-09-11 12:19:35 -0400

    Added entity abstract class

---
+++ src/entity.py
@@ -0,0 +1,27 @@
+
+Offset_x, Offset_y = 0, 0
+
+def draw_box(canvas, x, y, width, height, color='black')
+  canvas.create_line(x, y, x + width, y, fill=color)
+  canvas.create_line(x, y, x, y + height, fill=color)
+  canvas.create_line(x + width, y, x + width, y + height, fill=color)
+  canvas.create_line(x, y + height, x + width, y + height, fill=color)
+
+class Entity:
+  def __init__(self, x, y, width, height, is_static=True, draw_func=draw_box):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.is_static = is_static
+    self.draw_func = draw_func
+
+  def draw(self, canvas):
+    self.draw_func(canvas, self.x, self.y, self.width, self.height)
+
+  def get_params(self):
+    x, y, width, height = self.x, self.y, self.width, self.height
+    if not self.is_static:
+      x += Offset_x
+      y += Offset_y
+    return x, y, width, height
