76f4a31622881b6a7cb12ad9db8c6f0fdeaacbc1
Gabriel Lee <gelos@localhost.localdomain>
2021-09-11 01:53:53 -0400

    Added type functionality

--- src/ipc.py
+++ src/ipc.py
@@ -7,11 +7,22 @@ Connection = None
 def send(data):
   Connection.sendall(data.encode())
 
+def send_list(*data):
+  send(','.join(list(map(str, data))))
+
 def get():
   data = ''
   while (byte := Connection.recv(1)) != b'\n':
     data += byte.decode()
   return data
+
+def get_list():
+  data = get()
+  return data.split(',')
+
+def get_mapped_list(func):
+  data = get_list()
+  return list(map(func, data))
 
 def start(port):
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
